a7a8c355c595f49468e58dbc9df16aa7a437f213
Atmel13 <Atmel13@gmail.com>
2017-04-13 23:01:15 +0300

    Add files via upload

---
+++ Hometask3/src/main/java/addCategoryTest.java
@@ -0,0 +1,30 @@
+import org.openqa.selenium.WebDriver;
+import java.util.Date;
+
+public class addCategoryTest extends baseScript {
+
+    private static String login = "webinar.test@gmail.com";
+    private static String password = "Xcg7299bnSmMuRLp9ITw";
+    private static String categoryName;
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Date date = new Date();
+        categoryName =  date.toString();
+
+        WebDriver driver = getConfiguredDriver();
+
+        GeneralActions actions = new GeneralActions(driver);
+
+        actions.login(login,password);
+
+        actions.createCategory(categoryName);
+
+        actions.filterByName(categoryName);
+
+        driver.quit();
+
+        //после этого теста, можно б было еще добавить очистку созданной категории (или всех категорий) через UI или API
+
+    }
+}
